cc0cc54e0aacb75ef3d83f082af28c502d29ac9f
tttd1us <tttd1us@gmail.com>
2026-04-04 10:06:05 +0000

    Initial commit

--- main.js
+++ main.js
@@ -7,17 +7,20 @@ generateBtn.addEventListener("click", () => {
 
 function generateNumbers() {
   numbersContainer.innerHTML = "";
-  const numbers = new Set();
-
-  while (numbers.size < 6) {
-    const randomNumber = Math.floor(Math.random() * 45) + 1;
-    numbers.add(randomNumber);
-  }
-
-  for (const number of numbers) {
-    const numberEl = document.createElement("div");
-    numberEl.classList.add("number");
-    numberEl.textContent = number;
-    numbersContainer.appendChild(numberEl);
+  for (let i = 0; i < 10; i++) {
+    const setContainer = document.createElement("div");
+    setContainer.classList.add("set");
+    const numbers = new Set();
+    while (numbers.size < 6) {
+      const randomNumber = Math.floor(Math.random() * 45) + 1;
+      numbers.add(randomNumber);
+    }
+    for (const number of numbers) {
+      const numberEl = document.createElement("div");
+      numberEl.classList.add("number");
+      numberEl.textContent = number;
+      setContainer.appendChild(numberEl);
+    }
+    numbersContainer.appendChild(setContainer);
   }
 }
